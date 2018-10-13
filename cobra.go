7536ace0662ff076ebba40040d18adfeb971221c
Oleg Yashchuk <oazoer@gmail.com>
2018-10-13 13:15:56 +0300

    Add cobra command

---
+++ cobra.go
@@ -0,0 +1,31 @@
+package goose
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func Command(dialect, dbUri string) *cobra.Command {
+	return &cobra.Command{
+		Use:   "migrate up/down/redo/status",
+		Short: "Migrate Database",
+		Args:  cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			c := args[0]
+			fmt.Println(args)
+			if err := SetDialect(dialect); err != nil {
+				log.Fatal(err)
+			}
+			db, err := sql.Open(dialect, dbUri)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			if err := Run(c, db, "."); err != nil {
+				log.Fatal(err)
+			}
+		},
+	}
+}
